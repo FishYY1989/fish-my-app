c06e46ff481537b3a185eba3d45dccec558375a3
yuyang <yuyang@ihealthlabs.com.cn>
2021-08-30 16:28:04 +0800

    5组件复合

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import ClassComponent from "./pages/ClassComponent";
 import FunctionComponent from "./pages/FunctionComponent";
+import HomePage from "./pages/HomePage";
 import SetStatePage from "./pages/SetStatePage";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassComponent /> */}
       {/* <FunctionComponent /> */}
-      <SetStatePage/>
+      {/* <SetStatePage/> */}
+      <HomePage/>
+      {/* <UserPage/> */}
     </div>
   );
 
